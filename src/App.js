8ca86e8e8284d45e821bc93c6dff398dd1977cdf
panasonikk <grebenyuk1507@gmail.com>
2022-08-31 17:00:53 +0300

    Integrated API

--- src/App.js
+++ src/App.js
@@ -1,60 +1,100 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Search from "./Search";
 import WeatherDetails from "./WeatherDetails";
+import { MutatingDots } from "react-loader-spinner";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <Search />
-            <div className="row">
-              <div className="col-auto">
-                <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
-                  alt=""
-                  id="main-icon"
-                />
+  let [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.mail.humidity,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="card">
+            <div className="card-body">
+              <Search />
+              <div className="row">
+                <div className="col-auto">
+                  <img
+                    src="http://openweathermap.org/img/wn/01d@2x.png"
+                    alt=""
+                    id="main-icon"
+                  />
+                </div>
+                <div className="col-6 header">
+                  <h1>
+                    <span id="temperature">{weatherData.temperature}</span>
+                    <span className="units">
+                      <a href="/" className="celsius-link active">
+                        °C
+                      </a>
+                      |
+                      <a href="/" className="fahrenheit-link">
+                        °F
+                      </a>
+                    </span>
+                  </h1>
+                  <h1 id="city">{weatherData.city}</h1>
+                </div>
+                <div className="col-auto">
+                  <WeatherDetails
+                    description={weatherData.description}
+                    humidity={weatherData.humidity}
+                    wind={weatherData.wind}
+                  />
+                </div>
               </div>
-              <div className="col-6 header">
-                <h1>
-                  <span id="temperature">22</span>
-                  <span className="units">
-                    <a href="index.html" className="celsius-link active">
-                      °C
-                    </a>
-                    |
-                    <a href="index.html" className="fahrenheit-link">
-                      °F
-                    </a>
-                  </span>
-                </h1>
-                <h1 id="city">Kyiv</h1>
-              </div>
-              <div className="col-auto">
-                <WeatherDetails />
-              </div>
+              <hr />
+              <div id="forecast-container"></div>
             </div>
-            <hr />
-            <div id="forecast-container"></div>
           </div>
         </div>
+        <footer>
+          This is an
+          <a
+            href="https://github.com/panasonnik/weather-forecast-react"
+            target="_blank"
+            rel="noreferrer"
+            className="github-link"
+          >
+            {" "}
+            open-source{" "}
+          </a>
+          code by Sofiia Hrebeniuk
+        </footer>
       </div>
-      <footer>
-        This is an
-        <a
-          href="https://github.com/panasonnik/weather-forecast-react"
-          target="_blank"
-          rel="noreferrer"
-          className="github-link"
-        >
-          {" "}
-          open-source{" "}
-        </a>
-        code by Sofiia Hrebeniuk
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "63116731662a94eebc651f7bb7447ea1";
+    let city = "Kyiv";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    /* return (
+      <MutatingDots
+        height="100"
+        width="100"
+        color="#6f4c5b"
+        secondaryColor="#6f4c5b"
+        radius="12.5"
+        ariaLabel="mutating-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    ); */
+    return "Loading...";
+  }
 }
--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-export default function WeatherDetails() {
+export default function WeatherDetails(props) {
   return (
     <ul className="WeatherDetails">
       <li id="date" className="bold-li"></li>
       <br />
       <li>
-        Humidity: <span id="humidity">80</span>%
+        Humidity: <span id="humidity">{props.humidity}</span>%
       </li>
       <li>
-        Wind speed: <span id="wind-speed">2</span> km/h
+        Wind speed: <span id="wind-speed">{props.wind}</span> km/h
       </li>
       <li id="description" className="bold-li">
-        Sunny
+        {props.description}
       </li>
     </ul>
   );
